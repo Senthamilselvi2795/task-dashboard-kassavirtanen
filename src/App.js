57762382d51242f99bf54bb870c561a9f8c49ac1
Senthamil Selvi <senthamilyadav@gmail.com>
2026-03-03 22:21:45 +0530

    feat: integrate tasks page with redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTasksRequest } from "./features/tasks/tasksSlice";
+import TaskForm from "./components/TaskForm";
+
 function App() {
-  return <h1>Tasks App</h1>;
+  const dispatch = useDispatch();
+  const { tasks, loading } = useSelector((state) => state.tasks);
+
+  useEffect(() => {
+    dispatch(fetchTasksRequest());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Tasks</h2>
+
+      <TaskForm />
+
+      {loading && <p>Loading...</p>}
+
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h4>{task.title}</h4>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
